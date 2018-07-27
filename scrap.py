147ed179c075375e6bf5ee9d6e12ebc604bf369a
karl <karlbooklover@gmail.com>
2018-07-26 20:33:57 -0400

    works

--- scrap.py
+++ scrap.py
@@ -1,3 +1,4 @@
+# Made with love by Karlpy
 import requests
 import csv
 import json
@@ -8,20 +9,16 @@ set_url = 'https://servicios.set.gov.py/eset-publico/ciudadano/recuperar'
 id_range = 10000000
 id = 1
 
-with open ('datos.csv','w') as csvfile:
+with open ('datos.csv','w',newline='') as csvfile:
    writer=csv.writer(csvfile)
+   writer.writerow(['cedula', 'nombres', 'apellidoPaterno', 'apellidoMaterno', 'nombreCompleto'])
    for id in range(id_range):
        payload = {"cedula": id}
        result = requests.get(set_url, params=payload)
        res_json = result.json()
        id+=1
-
        if(res_json["presente"] == False):
             print("No existe el nro de cedula")
        else:
-           line = ""
-           for key,value in res_json['resultado'].items():
-                line += [key, value]
-           print(line)
-           writer.writerow(line)
-       
+           print(res_json["resultado"])
+           writer.writerow(res_json["resultado"].values())
