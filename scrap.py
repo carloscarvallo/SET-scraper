c29e5a9e1a1fd1badba6565a19246b075e1b23e7
carloscarvallo <carloscarvallo@protonmail.com>
2018-07-26 21:37:29 -0400

    use autoincrement and set switch for sleep

--- scrap.py
+++ scrap.py
@@ -2,23 +2,26 @@
 import requests
 import csv
 import json
+import time
 
 set_url = 'https://servicios.set.gov.py/eset-publico/ciudadano/recuperar'
 
 # loop through all URLs
-id_range = 10000000
-id = 1
+start_id = 1
+last_id = 100000
+# set sleep time between reqs, otherwise set to 0
+sleep_time = 0.800
 
 with open ('datos.csv','w',newline='') as csvfile:
-   writer=csv.writer(csvfile)
-   writer.writerow(['cedula', 'nombres', 'apellidoPaterno', 'apellidoMaterno', 'nombreCompleto'])
-   for id in range(id_range):
-       payload = {"cedula": id}
-       result = requests.get(set_url, params=payload)
-       res_json = result.json()
-       id+=1
-       if(res_json["presente"] == False):
+    writer=csv.writer(csvfile)
+    writer.writerow(['cedula', 'nombres', 'apellidoPaterno', 'apellidoMaterno', 'nombreCompleto'])
+    for id in range(start_id, last_id+1):
+        payload = {"cedula": id}
+        result = requests. get(set_url, params=payload)
+        res_json = result.json()
+        if(res_json["presente"] == False):
             print("No existe el nro de cedula")
-       else:
-           print(res_json["resultado"])
-           writer.writerow(res_json["resultado"].values())
+        else:
+            print(res_json["resultado"])
+            writer.writerow(res_json["resultado"].values())
+        time.sleep(sleep_time)
